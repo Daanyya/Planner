62ae91f9754c52b3c9267d0f36a34d02866b7618
Danya <danila.yrinx@mail.ru>
2023-01-08 13:35:32 +0400

    add hello to file PlannerApplication.java

--- src/main/java/com/example/Planner/PlannerApplication.java
+++ src/main/java/com/example/Planner/PlannerApplication.java
@@ -9,6 +9,7 @@ public class PlannerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlannerApplication.class, args);
+		System.out.println("Hello, world!");
 	}
 
 }
